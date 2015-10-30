07da87b6894847a8aad96269e4d8b823c89afbda
Brian <Brian.Pinto@quinnipiac.edu>
2015-10-30 00:20:57 +0000

    Add required user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import patterns, include, url
 from .views import *
 
@@ -5,12 +6,12 @@ urlpatterns = patterns('',
                       url(r'^$', Home.as_view(), name='home'),
                        url(r'^user/', include('registration.backends.simple.urls')),
                        url(r'^user/', include('django.contrib.auth.urls')),
-                       url(r'^movie/create/$', MovieCreateView.as_view(), name='movie_create'),
-                       url(r'movie/$', MovieListView.as_view(), name='movie_list'),
-                       url(r'^movie/(?P<pk>\d+)/$', MovieDetailView.as_view(), name='movie_detail'),
-                       url(r'^movie/update/(?P<pk>\d+)/$', MovieUpdateView.as_view(), name='movie_update'),
-                       url(r'^movie/delete/(?P<pk>\d+)/$', MovieDeleteView.as_view(), name='movie_delete'),
-                       url(r'^movie/(?P<pk>\d+)/review/create/$', ReviewCreateView.as_view(), name='review_create'),
-                       url(r'^movie/(?P<movie_pk>\d+)/review/update/(?P<review_pk>\d+)/$', ReviewUpdateView.as_view(), name='review_update'),
-                       url(r'^movie/(?P<movie_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', ReviewDeleteView.as_view(), name='review_delete'),
+                       url(r'^movie/create/$', login_required(MovieCreateView.as_view()), name='movie_create'),
+                       url(r'movie/$', login_required(MovieListView.as_view()), name='movie_list'),
+                       url(r'^movie/(?P<pk>\d+)/$', login_required(MovieDetailView.as_view()), name='movie_detail'),
+                       url(r'^movie/update/(?P<pk>\d+)/$', login_required(MovieUpdateView.as_view()), name='movie_update'),
+                       url(r'^movie/delete/(?P<pk>\d+)/$', login_required(MovieDeleteView.as_view()), name='movie_delete'),
+                       url(r'^movie/(?P<pk>\d+)/review/create/$', login_required(ReviewCreateView.as_view()), name='review_create'),
+                       url(r'^movie/(?P<movie_pk>\d+)/review/update/(?P<review_pk>\d+)/$', login_required(ReviewUpdateView.as_view()), name='review_update'),
+                       url(r'^movie/(?P<movie_pk>\d+)/review/delete/(?P<review_pk>\d+)/$', login_required(ReviewDeleteView.as_view()), name='review_delete'),
                       )
